faae00e6ad29771d713e44566e6a2154459c1842
Unknown <smauricioj@gmail.com>
2018-10-22 15:02:12 -0300

    Mudança na estrutura de Sch

--- auction/src/agt/driver.asl
+++ auction/src/agt/driver.asl
@@ -1,11 +1,20 @@
 // Agent driver in project auction
 
-/* Initial beliefs and rules */
+/* Initial beliefs and rules
+ * schedule form -> [ [ci, ti, di, xi, yi] | for all i in schedule ]
+ *     onde : c = cliente
+ *            t = instante real de atendimento
+ *            d = instante desejado de atendimento
+ * 			  x, y = posição cartesiana de atendimento 
+ * */
 
 capacity(4).
 init_pos([5,5]).
 auctions_in_place([]).
-schedule([[0,0,5,5]]).
+schedule([["c0",0,0,5,5]]).
+schedule_test([["c0",0,0,5,5],
+	           ["c1",2,2,5,7], ["c2",6,5,8,7],
+	           ["c2",10.6,0,5,5], ["c1",11.6,0,5,5]]).
 
 /* Initial goals */
 
@@ -13,14 +22,14 @@ schedule([[0,0,5,5]]).
 
 +auction(St, [X, Y], KT, DT)[source(A)] : true
 	<-  ?schedule(Sch);
+		?schedule_test(Sch_t)
 		?capacity(C);
 		?init_pos(Ip);
 		Value = math.random * 100 + 10;
 		// TODO: custo de inserção
-		//	jia.schedule_cost(Sch, Ip, C, St, [X, Y], KT, DT, Cost)
-		jia.schedule_cost([["oi",2,5,5],["banana",5,8,6],["ultimo",7,1,1]],[5,5],4,"drop",[1,1],1,1,Result);
+		jia.schedule_cost(Sch_t, [5,5], 4, "drop", [1,1], 1, 1, Result);
 		.print(Result)
-		.send(A,tell,bid(Value));
+		.send(A,tell,bid(Result));
 		?auctions_in_place(L);
 		.concat(L,[A],NL);
 		.abolish(auctions_in_place(_));
@@ -34,7 +43,7 @@ schedule([[0,0,5,5]]).
 		?init_pos(Ip);
 		
 		// TODO: inserção de novos pedidos
-		//	 jia.schedule_update(Sch, Ip, St, [X, Y], KT, DT, NewSch)
+		//	 jia.schedule_update(Sch, St, [X, Y], DT, NewSch)
 		
 		!!remove_auction_in_place(A)
 		.
--- auction/src/agt/jia/schedule_cost.java
+++ auction/src/agt/jia/schedule_cost.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 @SuppressWarnings("serial")
 public class schedule_cost extends DefaultInternalAction {
-	//	 jia.schedule_cost(Sch, C, Ip, St, [X, Y], KT, DT, Result)
+	//	 jia.schedule_cost(Sch, C, St, [X, Y], KT, DT, Result)
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
@@ -20,15 +20,16 @@ public class schedule_cost extends DefaultInternalAction {
         	throw new JasonException("Last argument of schedule_cost is not a variable");
         }
         
-        int i_pe = 0; //indice do primeiro evento
-        int n_e = 0; //total de eventos
-        NumberTerm kt = (NumberTerm)args[5];
-        NumberTerm dt = (NumberTerm)args[6];
-    	for (Term t: (ListTerm)args[0]) { // Todos os eventos da agenda
-            ListTerm event = (ListTerm)t; // Um evento específico
+        int i_pe = 0;                        //indice do primeiro evento
+        int n_e = 0;                         //total de eventos
+        NumberTerm kt = (NumberTerm)args[5]; // instante conhecido (agora)
+        NumberTerm dt = (NumberTerm)args[6]; // instante desejado (no futuro)
+    	for (Term t: (ListTerm)args[0]) {    // Todos os eventos da agenda
+            ListTerm event = (ListTerm)t;    // Um evento específico
             NumberTerm et = (NumberTerm)event.get(1); // instante do evento
             if ( et.solve() < kt.solve() || et.solve() < dt.solve()) {
-            	// se é passado OU anterior ao desejado, aumenta o index do primeiro evento 
+            	/* se é passado OU anterior ao desejado,
+            	   aumenta o index do primeiro evento    */ 
             	i_pe += 1;
             }
             n_e += 1;
@@ -36,13 +37,11 @@ public class schedule_cost extends DefaultInternalAction {
     	
     	int n_estrela = n_e - i_pe; // número de espaços disponíveis
     	double inf = Double.POSITIVE_INFINITY; // infinito
-    	double minValue = inf; // menor valor até agora
-    	double actValue = 0; // valor atual
+    	double minValue = inf;      // menor valor até agora
+    	double actValue = 0;        // valor atual
     	Random rand = new Random(); //máquina de randoms
-    	for (int i = 0; i < n_estrela; i ++) {
-    		// número de linhas
-    		for (int j = 0; j < n_estrela; j ++) {
-    			// número de colunas
+    	for (int i = 0; i < n_estrela; i ++) {		// número de linhas
+    		for (int j = 0; j < n_estrela; j ++) {	// número de colunas
     			if (j < i) {
     				// a matriz deve ser triangular superior
     				actValue = inf;
@@ -51,8 +50,6 @@ public class schedule_cost extends DefaultInternalAction {
     				// TODO: calculo do kappa_ij
     				actValue = rand.nextInt(50)+1;
     			}
-    			
-//    			System.out.println(actValue);
     			
     			if (actValue < minValue) {
     				// se o atual é menor do que o menor até agora, atualiza
