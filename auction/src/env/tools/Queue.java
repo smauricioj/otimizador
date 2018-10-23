a9d368761794560d53fe5c2a706ee9e781765399
Unknown <smauricioj@gmail.com>
2018-10-23 14:01:27 -0300

    Inclusão do ambiente

---
+++ auction/src/env/tools/Queue.java
@@ -0,0 +1,28 @@
+// CArtAgO artifact code for project auction
+
+package tools;
+
+import cartago.Artifact;
+import cartago.OPERATION;
+import cartago.INTERNAL_OPERATION;
+import cartago.ObsProperty;
+
+public class Queue extends Artifact {
+	
+	void init() {
+		defineObsProperty("next", 0);
+		execInternalOp("initial_inc");
+	}
+
+	@OPERATION
+	void inc() {
+		ObsProperty prop = getObsProperty("next");		
+		prop.updateValue(prop.intValue()+1);
+	}
+	
+	@INTERNAL_OPERATION
+	void initial_inc() {
+		await_time(100);
+		inc();
+	}
+}
