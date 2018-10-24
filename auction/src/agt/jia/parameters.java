e02882246287a1e179138d5e839f62f37a48fbce
Unknown <smauricioj@gmail.com>
2018-10-24 14:12:39 -0300

    :sparkles: Inserção de eventos no Sch

--- auction/src/agt/jia/parameters.java
+++ auction/src/agt/jia/parameters.java
@@ -5,6 +5,7 @@ public class parameters{
 	public static final int VEHICLE_CAPACITY = 4;
 	public static final int VEHICLE_INITIAL_POSITION_X = 5;
 	public static final int VEHICLE_INITIAL_POSITION_Y = 5;
+	public static final double SERVICE_TIME = 1;
 	
 	/** Control parameters **/
 	public static final double CONTROL_GAMMA = 0.5;
--- auction/src/agt/jia/schedule_cost.java
+++ auction/src/agt/jia/schedule_cost.java
@@ -6,31 +6,68 @@ import jason.*;
 import jason.asSemantics.*;
 import jason.asSyntax.*;
 import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class schedule_cost extends DefaultInternalAction {
 	
 	private String service_type = "";
+	private String client_name = "_";
 	private double service_position_x = 0;
 	private double service_position_y = 0;
 	private double desired_time = 0;
 	private double known_time = 0;
 	
-	private double kappa_ij(ListTerm Sch, int i, int j) {
-    	Random rand = new Random(); //mï¿½quina de randoms
+	private double kappa_ij(ListTerm Sch, int i, int j) throws NoValueException {
+    	Random rand = new Random(); //máquina de randoms
     	double inf = Double.POSITIVE_INFINITY; // infinito
     	
-    	ListTerm Sch_teste = Sch.cloneLT();
-    	int count = 0;
-    	for (Term t : Sch_teste) {
-    		ListTerm event = (ListTerm)t;
+    	ListTerm new_event_i = ASSyntax.createList(ASSyntax.createString(this.client_name),
+    											   ASSyntax.createNumber(0),
+    			                                   ASSyntax.createNumber(this.desired_time));
+    	
+    	ListTerm new_event_j = ASSyntax.createList(ASSyntax.createString(this.client_name),
+				   							       ASSyntax.createNumber(0),
+								                   ASSyntax.createNumber(this.desired_time));
+//    	System.out.println(this.service_type);
+//    	System.out.println("\"drop\"");
+    	if ( this.service_type.equals("\"drop\"") ) {
+    		new_event_i.add(ASSyntax.createNumber(parameters.VEHICLE_INITIAL_POSITION_X));
+    		new_event_i.add(ASSyntax.createNumber(parameters.VEHICLE_INITIAL_POSITION_Y));
     		
-    		count += 1;
+    		new_event_j.add(ASSyntax.createNumber(this.service_position_x));
+    		new_event_j.add(ASSyntax.createNumber(this.service_position_y));    		
+    	} else {
+    		new_event_j.add(ASSyntax.createNumber(parameters.VEHICLE_INITIAL_POSITION_X));
+    		new_event_j.add(ASSyntax.createNumber(parameters.VEHICLE_INITIAL_POSITION_Y));
+    		
+    		new_event_i.add(ASSyntax.createNumber(this.service_position_x));
+    		new_event_i.add(ASSyntax.createNumber(this.service_position_y));    		
+    	}
+    	ListTerm Sch_novo = Sch.cloneLT();
+    	Sch_novo.add(i+1, new_event_i);
+    	Sch_novo.add(j+2, new_event_j);
+//    	System.out.println(" "+i+" "+j+" ");
+    	for ( int index = i+1; index < Sch_novo.size(); index++ ) {
+    		ListTerm event = (ListTerm)Sch_novo.get(index);
+    		ListTerm last_event = (ListTerm)Sch_novo.get(index-1);
+
+    		NumberTerm t0Term = (NumberTerm)last_event.get(1);
+    		NumberTerm x0Term = (NumberTerm)last_event.get(3);
+    		NumberTerm y0Term = (NumberTerm)last_event.get(4);
+    		NumberTerm x1Term = (NumberTerm)event.get(3);
+    		NumberTerm y1Term = (NumberTerm)event.get(4);
+    		double x0 = x0Term.solve();
+			double y0 = y0Term.solve();
+			double x1 = x1Term.solve();
+			double y1 = y1Term.solve();
+    		double tempo_viagem = Math.round( Math.sqrt( Math.pow(x0-x1, 2) + Math.pow(y0-y1, 2) ) );
+    		double instante_atendimento = t0Term.solve() + parameters.SERVICE_TIME + tempo_viagem;
+			event.set(1, ASSyntax.createNumber(instante_atendimento));
+    		
+    		Sch_novo.set(index, event);
     	}
     	
-//    	System.out.println(" "+i+" "+j+" "+Sch_teste);
+    	System.out.println(Sch_novo);
     	
 //    	Sch_teste.add(1, (Term)1);
 		
@@ -47,19 +84,17 @@ public class schedule_cost extends DefaultInternalAction {
         	throw new JasonException("Last argument of schedule_cost is not a variable");
         }
         
-//        System.out.println(parameters.VEHICLE_CAPACITY);
-        
         int i_pe = 0;                        // indice do primeiro evento
         int n_e = 0;                         // total de eventos
         ListTerm Sch = (ListTerm)args[0];    // agendamento atual
         
-        StringTerm St = (StringTerm)args[1]; // tipo de serviï¿½o
+        StringTerm St = (StringTerm)args[1]; // tipo de serviço
         this.service_type = St.toString();
         
-        NumberTerm Pos_x = (NumberTerm)args[2];    // posiï¿½ï¿½o x do novo pedido
+        NumberTerm Pos_x = (NumberTerm)args[2];    // posição x do novo pedido
         this.service_position_x = Pos_x.solve();
         
-        NumberTerm Pos_y = (NumberTerm)args[3];    // posiï¿½ï¿½o y do novo pedido
+        NumberTerm Pos_y = (NumberTerm)args[3];    // posição y do novo pedido
         this.service_position_y = Pos_y.solve();
         
         NumberTerm Kt = (NumberTerm)args[4]; // instante conhecido (agora)
@@ -69,40 +104,40 @@ public class schedule_cost extends DefaultInternalAction {
         this.desired_time = Dt.solve();
         
     	for (Term t: Sch) {    // Todos os eventos da agenda
-            ListTerm event = (ListTerm)t;    // Um evento especï¿½fico
+            ListTerm event = (ListTerm)t;    // Um evento específico
             NumberTerm et = (NumberTerm)event.get(1); // instante do evento
             if ( et.solve() < Kt.solve() || et.solve() < Dt.solve()) {
-            	/* se ï¿½ passado OU anterior ao desejado,
+            	/* se é passado OU anterior ao desejado,
             	   aumenta o index do primeiro evento    */
             	i_pe += 1;
             }
             n_e += 1;
         }
     	
-    	int n_estrela = n_e - i_pe; // nï¿½mero de espaï¿½os disponï¿½veis
-    	if ( n_e == 1 ) { // caso primeira inserï¿½ï¿½o do motorista (ï¿½ feio, eu sei)
+    	int n_estrela = n_e - i_pe; // número de espaços disponíveis
+    	if ( n_e == 1 ) { // caso primeira inserção do motorista (é feio, eu sei)
     		n_estrela = 1;
     		i_pe = 0;
     	}
-//    	System.out.println("n_estrela ï¿½ "+n_estrela+" e  os outros sï¿½o "+n_e+" "+i_pe);
+//    	System.out.println("n_estrela é "+n_estrela+" e  os outros são "+n_e+" "+i_pe);
     	double inf = Double.POSITIVE_INFINITY; // infinito
-    	double minValue = inf;      // menor valor atï¿½ agora
+    	double minValue = inf;      // menor valor até agora
     	double actValue = 0;        // valor atual
-    	for (int i = 0; i < n_estrela; i ++) {		// nï¿½mero de linhas
-    		for (int j = 0; j < n_estrela; j ++) {	// nï¿½mero de colunas
+    	for (int i = 0; i < n_estrela; i ++) {		// número de linhas
+    		for (int j = 0; j < n_estrela; j ++) {	// número de colunas
     			if (j >= i){
-    				// calcula o custo da inserï¿½ï¿½o na triangular superior
+    				// calcula o custo da inserção na triangular superior
     				actValue = this.kappa_ij(Sch, i+i_pe, j+i_pe);
     			}
     			
     			if (actValue < minValue) {
-    				// se o atual ï¿½ menor do que o menor atï¿½ agora, atualiza
+    				// se o atual é menor do que o menor até agora, atualiza
     				minValue = actValue;
     			}
     		}
     	}
 
-        // unifica o resultado com a variï¿½vel passada
+        // unifica o resultado com a variável passada
     	return un.unifies(args[args.length-1], ASSyntax.createNumber(minValue));
     }
 }
