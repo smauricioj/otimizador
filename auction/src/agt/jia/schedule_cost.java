786b992a27ba3891562a4262d5122e596eca3e54
Unknown <smauricioj@gmail.com>
2018-10-29 15:56:52 -0300

    :art: :sparkles: Cálculo de métricas

--- auction/src/agt/jia/schedule_cost.java
+++ auction/src/agt/jia/schedule_cost.java
@@ -6,6 +6,8 @@ import jason.*;
 import jason.asSemantics.*;
 import jason.asSyntax.*;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class schedule_cost extends DefaultInternalAction {
@@ -17,21 +19,68 @@ public class schedule_cost extends DefaultInternalAction {
 	private double desired_time = 0;
 	private double known_time = 0;
 	
+	private double travel_distance(ListTerm event, ListTerm last_event) throws NoValueException {
+		
+		NumberTerm x0Term = (NumberTerm)last_event.get(3);
+		NumberTerm y0Term = (NumberTerm)last_event.get(4);
+		NumberTerm x1Term = (NumberTerm)event.get(3);
+		NumberTerm y1Term = (NumberTerm)event.get(4);
+		double x0 = x0Term.solve();
+		double y0 = y0Term.solve();
+		double x1 = x1Term.solve();
+		double y1 = y1Term.solve();
+		
+		return Math.round( Math.sqrt( Math.pow(x0-x1, 2) + Math.pow(y0-y1, 2) ) );
+		
+	}
+	
+	private double[] metrics(ListTerm Sch) throws NoValueException {
+		
+		double[] metrics = new double[3];
+		List<String> cliente_list = new ArrayList<String>();
+		List<Double> pick_up_list = new ArrayList<Double>();		
+		
+		for (int index = 0; index < Sch.size(); index++) {
+    		ListTerm event = (ListTerm)Sch.get(index);
+			if (index != 0) {
+	    		ListTerm last_event = (ListTerm)Sch.get(index-1);
+	    		
+	    		metrics[0] += this.travel_distance(event, last_event);
+			}
+			StringTerm cliente = (StringTerm)event.get(0);
+			if(!cliente_list.contains(cliente.toString())) {
+				NumberTerm atendimento = (NumberTerm)event.get(1);
+				NumberTerm desejado = (NumberTerm)event.get(2);
+				pick_up_list.add(atendimento.solve());
+				cliente_list.add(cliente.toString());
+				
+				metrics[1] += atendimento.solve() - desejado.solve();
+			} else {
+				int cliente_index = cliente_list.indexOf(cliente.toString());
+				NumberTerm atendimento = (NumberTerm)event.get(1);
+				double drop_off = atendimento.solve();
+				double pick_up = pick_up_list.get(cliente_index);
+				
+				metrics[2] += drop_off - pick_up;				
+			}
+			
+		}
+		
+		
+		
+		return metrics;
+	}
 	
 	private double kappa_ij(ListTerm Sch, int i, int j) throws NoValueException {		
-		/** Calcula custo da inserï¿½ï¿½o do pedido em i e j **/
+		/** Calcula custo da inserção do pedido em i e j **/
 		
-    	Random rand = new Random(); //mï¿½quina de randoms
+    	Random rand = new Random(); //máquina de randoms
     	double inf = Double.POSITIVE_INFINITY; // infinito
     	
-    	/** Os eventos no Sch sï¿½o definidos por:
+    	/** Os eventos no Sch são definidos por:
     	 *  	[cliente, atendimento, desejado, x, y]
-    	 *  Abaixo, dois eventos (buscar e levar) sï¿½o criados
+    	 *  Abaixo, dois eventos (buscar e levar) são criados
     	 *  de acordo com o cliente. **/
-    	
-	private double kappa_ij(ListTerm Sch, int i, int j) throws NoValueException {
-    	Random rand = new Random(); //mï¿½quina de randoms
-    	double inf = Double.POSITIVE_INFINITY; // infinito
     	
     	ListTerm new_event_i = ASSyntax.createList(ASSyntax.createString(this.client_name),
     											   ASSyntax.createNumber(0),
@@ -55,8 +104,8 @@ public class schedule_cost extends DefaultInternalAction {
     		new_event_i.add(ASSyntax.createNumber(this.service_position_y));    		
     	}
     	
-    	/** Os testes sï¿½o realizados sobre um clone do agendamento
-    	 *  Apï¿½s a insersï¿½o em i e j, todos os eventos apï¿½s i sï¿½o
+    	/** Os testes são realizados sobre um clone do agendamento
+    	 *  Após a insersão em i e j, todos os eventos após i são
     	 *  atualizados, modificando seu tempo de atendimento de
     	 *  acordo com as distancias **/
     	
@@ -69,24 +118,14 @@ public class schedule_cost extends DefaultInternalAction {
     		ListTerm last_event = (ListTerm)Sch_novo.get(index-1);
 
     		NumberTerm t0Term = (NumberTerm)last_event.get(1);
-    		NumberTerm x0Term = (NumberTerm)last_event.get(3);
-    		NumberTerm y0Term = (NumberTerm)last_event.get(4);
-    		NumberTerm x1Term = (NumberTerm)event.get(3);
-    		NumberTerm y1Term = (NumberTerm)event.get(4);
-    		double x0 = x0Term.solve();
-			double y0 = y0Term.solve();
-			double x1 = x1Term.solve();
-			double y1 = y1Term.solve();
-    		double tempo_viagem = Math.round( Math.sqrt( Math.pow(x0-x1, 2) + Math.pow(y0-y1, 2) ) );
+    		double tempo_viagem = this.travel_distance(event, last_event);
     		double instante_atendimento = t0Term.solve() + parameters.SERVICE_TIME + tempo_viagem;
 			event.set(1, ASSyntax.createNumber(instante_atendimento));
     		
     		Sch_novo.set(index, event);
     	}
     	
-    	System.out.println(Sch_novo);
-    	
-//    	Sch_teste.add(1, (Term)1);
+//    	System.out.println(Sch_novo);
 		
 		return rand.nextInt(50)+1;
 	}
@@ -105,13 +144,13 @@ public class schedule_cost extends DefaultInternalAction {
         int n_e = 0;                         // total de eventos
         ListTerm Sch = (ListTerm)args[0];    // agendamento atual
         
-        StringTerm St = (StringTerm)args[1]; // tipo de serviï¿½o
+        StringTerm St = (StringTerm)args[1]; // tipo de serviço
         this.service_type = St.toString();
         
-        NumberTerm Pos_x = (NumberTerm)args[2];    // posiï¿½ï¿½o x do novo pedido
+        NumberTerm Pos_x = (NumberTerm)args[2];    // posição x do novo pedido
         this.service_position_x = Pos_x.solve();
         
-        NumberTerm Pos_y = (NumberTerm)args[3];    // posiï¿½ï¿½o y do novo pedido
+        NumberTerm Pos_y = (NumberTerm)args[3];    // posição y do novo pedido
         this.service_position_y = Pos_y.solve();
         
         NumberTerm Kt = (NumberTerm)args[4]; // instante conhecido (agora)
@@ -121,40 +160,40 @@ public class schedule_cost extends DefaultInternalAction {
         this.desired_time = Dt.solve();
         
     	for (Term t: Sch) {    // Todos os eventos da agenda
-            ListTerm event = (ListTerm)t;    // Um evento especï¿½fico
+            ListTerm event = (ListTerm)t;    // Um evento específico
             NumberTerm et = (NumberTerm)event.get(1); // instante do evento
             if ( et.solve() < Kt.solve() || et.solve() < Dt.solve()) {
-            	/* se ï¿½ passado OU anterior ao desejado,
+            	/* se é passado OU anterior ao desejado,
             	   aumenta o index do primeiro evento    */
             	i_pe += 1;
             }
             n_e += 1;
         }
     	
-    	int n_estrela = n_e - i_pe; // nï¿½mero de espaï¿½os disponï¿½veis
-    	if ( n_e == 1 ) { // caso primeira inserï¿½ï¿½o do motorista (ï¿½ feio, eu sei)
+    	int n_estrela = n_e - i_pe; // número de espaços disponíveis
+    	if ( n_e == 1 ) { // caso primeira inserção do motorista (é feio, eu sei)
     		n_estrela = 1;
     		i_pe = 0;
     	}
-//    	System.out.println("n_estrela ï¿½ "+n_estrela+" e  os outros sï¿½o "+n_e+" "+i_pe);
+//    	System.out.println("n_estrela é "+n_estrela+" e  os outros são "+n_e+" "+i_pe);
     	double inf = Double.POSITIVE_INFINITY; // infinito
-    	double minValue = inf;      // menor valor atï¿½ agora
+    	double minValue = inf;      // menor valor até agora
     	double actValue = 0;        // valor atual
-    	for (int i = 0; i < n_estrela; i ++) {		// nï¿½mero de linhas
-    		for (int j = 0; j < n_estrela; j ++) {	// nï¿½mero de colunas
+    	for (int i = 0; i < n_estrela; i ++) {		// número de linhas
+    		for (int j = 0; j < n_estrela; j ++) {	// número de colunas
     			if (j >= i){
-    				// calcula o custo da inserï¿½ï¿½o na triangular superior
+    				// calcula o custo da inserção na triangular superior
     				actValue = this.kappa_ij(Sch, i+i_pe, j+i_pe);
     			}
     			
     			if (actValue < minValue) {
-    				// se o atual ï¿½ menor do que o menor atï¿½ agora, atualiza
+    				// se o atual é menor do que o menor até agora, atualiza
     				minValue = actValue;
     			}
     		}
     	}
 
-        // unifica o resultado com a variï¿½vel passada
+        // unifica o resultado com a variável passada
     	return un.unifies(args[args.length-1], ASSyntax.createNumber(minValue));
     }
 }
